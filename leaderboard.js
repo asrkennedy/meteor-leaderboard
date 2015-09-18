53559a0dd03c32cde14e580f769ded4c20afe1eb
Andrea Kennedy <andreakennedy@Mac-Pro.local>
2015-09-18 16:05:23 +0100

    adds helpers and events

--- leaderboard.js
+++ leaderboard.js
@@ -2,14 +2,23 @@ PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
     // this code only runs on the client
+
+    // HELPERS
     Template.leaderboard.helpers({
       'player': function(){
         return PlayersList.find() 
       },
       'otherHelperFunction': function(){
         return "Some other function"
-    }
-  })
+      }
+    })
+
+    // EVENTS
+    Template.leaderboard.events({
+      'click': function(){
+        console.log('You clicked something!')
+      }
+    })
 }
 
 if(Meteor.isServer){
