4f04d236d5204c343806aeb9154bb2bdcb33d49e
Andrea Kennedy <andreakennedy@Mac-Pro.local>
2015-09-18 17:29:08 +0100

    adds selected class with db call

--- leaderboard.js
+++ leaderboard.js
@@ -8,16 +8,21 @@ if(Meteor.isClient){
       'player': function(){
         return PlayersList.find() 
       },
-      'otherHelperFunction': function(){
-        return "Some other function"
+      'selectedClass': function(){
+        var playerId = this._id;
+        var selectedPlayer = Session.get('selectedPlayer');
+        if(playerId == selectedPlayer) {
+          return "selected"
+        }
       }
     })
 
     // EVENTS
     Template.leaderboard.events({
-      'click': function(){
-        console.log('You clicked something!')
-      }
+      'click .player': function(){
+        var playerId = this._id
+        Session.set('selectedPlayer', playerId);
+      },
     })
 }
 
